fdeec1aea654b5ab1e576f90e4b4d1374a3cbe6e
Claudio Gil <claudio.f.gil@gmail.com>
2019-03-08 02:16:42 +0000

    Updated some users from twitter threads

--- src/data/users.js
+++ src/data/users.js
@@ -1,6 +1,44 @@
 export default {
   "1GPGgwyDFpyWNNYG9b75rewiCULGNyzef8": {
-    twitter: "https://twitter.com/LeeFlemingster/status/1103568864451932160",
-    reddit: null
+    twitter: "https://twitter.com/LeeFlemingster/status/1103568864451932160"
+  },
+  "1KxRdCeSnLtmqsyepCc8Sm2rJcZY8vQtqV": {
+    twitter: "https://twitter.com/zquestz/status/1103456473244418054"
+  },
+  "1Py7rYoELQy41Td5Kzqse86gF5Tn4hYoM8": {
+    twitter: "https://twitter.com/CollinEnstad/status/1103459641248411649"
+  },
+  "1Ep6tzMUmme4xqRLLdwGasNN2YMn2kz96D": {
+    twitter: "https://twitter.com/Don_wonton/status/1103523885381750784"
+  },
+  "12FdefTCU3jAajGv9x1wYCaShLm5BefUNK": {
+    twitter: "https://twitter.com/rob_mose/status/1103531856874496000"
+  },
+  "1GjbtxLsgqzwrMGn6p9rPecMUQMMFkyLmb": {
+    twitter: "https://twitter.com/im_uname/status/1103534771810951168"
+  },
+  "12fyJGfTuA2D7mwKKdqRDeErxBq1jJqxc6": {
+    twitter: "https://twitter.com/JettScythe/status/1103537308286898176"
+  },
+  "1LeVpWm9QotUVx3dkSycUXZsM7Y8nw1Bh8": {
+    twitter: "https://twitter.com/Wecx_/status/1103547584952512512"
+  },
+  "1HSjUBkTKrL2KykmEKGsQGL2Jd8wRdpAKo": {
+    twitter: "https://twitter.com/paOolCoin/status/1103556037016092672"
+  },
+  "1XmQo8qmEpsy9LTy65Cr3zA3ySvZDXKjS": {
+    twitter: "https://twitter.com/Anthony05064687"
+  },
+  "1FQ29FuzTkDRcnCb4zaDL3PAzo3QLpZxho": {
+    twitter: "https://twitter.com/Klakurka/status/1103680413266927621"
+  },
+  "1NKfAXtmMW3LXcsgnrY5AhSSpkPkg2wd9C": {
+    twitter: "https://twitter.com/DavidShares/status/1103688119696703488"
+  },
+  "15Q6b15NeRvVWmj2McpmMKWuNGr6Mj3B9u": {
+    twitter: "https://twitter.com/maplesyrupsuckr"
+  },
+  "1DJN3FxJkCJSVX99FadxQ8pcD6epmnAyqJ": {
+    twitter: "https://twitter.com/BionicHacker/status/1103612969361977344"
   }
 };

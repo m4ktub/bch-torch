322c3c27fe55b86a8d6ba4b6cdf8e24e6222ff62
Claudio Gil <claudio.f.gil@gmail.com>
2019-03-10 09:33:45 +0000

    Changed logo for the SLP torch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <section id="header">
-      <img height="32" src="./images/torch-128.png">
+      <img height="128" src="./images/torch-color-565.png">
       <h1>The Bitcoin Cash Torch</h1>
       <p>Follow the trail of the first (and only) Bitcoin Cash torch.</p>
     </section>
